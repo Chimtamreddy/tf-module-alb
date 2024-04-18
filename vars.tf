bfe7fbba9828a6378ff75b7c17b3e96f8c7961d8
Chimtamreddy <kr7348202@gmail.com>
2024-04-18 18:51:26 +0530

    roboshop terraform

---
+++ vars.tf
@@ -0,0 +1,9 @@
+
+variable "tags" {}
+variable "env" {}
+variable "internal" {}
+variable "lb_type" {}
+variable "vpc_id" {}
+variable "sg_port" {}
+variable "sg_ingress_cidr" {}
+variable "subnets" {}
